6d222452daaff11cdc9b264ba085f0a0fa3e28ee
Sreehari <sreehari@activesphere.com>
2013-11-28 14:35:05 +0530

    validate share

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,6 +2,9 @@ module Mongoid
   class Share
     include Mongoid::Document
 
+    validates :sharer, presence: true
+    validates :sharable, presence: true
+
     belongs_to :sharer, :class_name => Socializer.user_class_name, :inverse_of => :shares
     belongs_to :sharable, :polymorphic => true
   end
